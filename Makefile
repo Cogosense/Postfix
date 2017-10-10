aaf947a79905f4e991e112519c9ea2ae88fe712d
Steve Williams <steve.williams@telxio.com>
2017-10-10 15:33:41 -0700

    add SMTP relay support amd AWS SES support

--- Makefile
+++ Makefile
@@ -4,33 +4,32 @@ REGISTRY = 053262612181.dkr.ecr.us-west-2.amazonaws.com
 IMAGE = $(REGISTRY)/postfix
 STAGE ?= dev
 
-all : build tag
-full : build-no-cache tag
+all : build
+full : build-no-cache
 
 build :
-	$(at)docker build -t $(IMAGE) src
+	$(at)docker build --pull -t $(IMAGE):$(STAGE) src
 
 build-no-cache :
-	$(at)docker build --no-cache -t $(IMAGE) src
+	$(at)docker build --pull --no-cache -t $(IMAGE):$(STAGE) src
 
 tag : build
-	$(at)if [ x"$(TAG)" != x ] ; then \
+	$(at)if [ x"$(TAG)" != xunknown ] ; then \
 		docker tag $(IMAGE) $(IMAGE):$(TAG) ; \
-		docker tag $(IMAGE) $(IMAGE):dev ; \
 	fi
 
 push : tag
 	$(at)if [ x"$(TAG)" != xunknown ] ; then \
 		if which -s aws ; then \
 			eval $$(aws ecr get-login --no-include-email --region us-west-2) ; \
-			docker push $(IMAGE):dev ; \
+			docker push $(IMAGE):$(STAGE) ; \
+			docker push $(IMAGE):$(TAG) ; \
 		else \
 			echo "AWS CLI must be installed to push images to AWS ECR". ; \
 			echo "https://aws.amazon.com/cli" ; \
 		fi ; \
 	fi
 
-devprd opsqa opsprd : push
-	docker tag $(IMAGE) $(IMAGE):$@
-	docker push $(IMAGE):$@
+devprd opsqa opsprd :
+	$(MAKE) STAGE=$@ build push
 
--- src/postfix.sh
+++ src/postfix.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 POSTFIX_RUNDIR=/var/spool/postfix
 [ -n "$POSTFIX_HOSTNAME" ] || { echo "set POSTFIX_HOSTNAME parameter in .env file" ; exit 1; }
-[ -n "$POSTFIX_RELAYHOST" ] || { echo "set POSTFIX_RELAYHOST parameter in .env file" ; exit 1; }
-if [ -n "$POSTFIX_AUTH_USERNAME" ] ; then
-    [ -n "$POSTFIX_AUTH_PASSWORD" ] || { echo "set POSTFIX_AUTH_PASSWORD parameter in .env file" ; exit 1; }
+if [ -n "$SES_AUTH_USERNAME" ] ; then
+    [ -n "$SES_AUTH_PASSWORD" ] || { echo "set POSTFIX_AUTH_PASSWORD parameter in .env file" ; exit 1; }
+    POSTFIX_RELAYHOST='127.0.0.1:2525'
+else
+    if [ -n "$POSTFIX_AUTH_USERNAME" ] ; then
+        [ -n "$POSTFIX_AUTH_PASSWORD" ] || { echo "set POSTFIX_AUTH_PASSWORD parameter in .env file" ; exit 1; }
+        [ -n "$POSTFIX_RELAYHOST" ] || { echo "set POSTFIX_RELAYHOST parameter in .env file" ; exit 1; }
+    fi
+    case $POSTFIX_RELAYHOST in
+        '') ;;
+        *:*)
+            hostpart=${POSTFIX_RELAYHOST%%:*}
+            portpart=${POSTFIX_RELAYHOST#*:}
+            POSTFIX_RELAYHOST="[$hostpart]"${portpart:+":$portpart"}
+            ;;
+        *)
+            POSTFIX_RELAYHOST="[$POSTFIX_RELAYHOST]"
+            ;;
+    esac
 fi
 
 if [ -d /dev/logger ] ; then
     ln -sf /dev/logger/log /dev/log
 fi
+
+#
+# start stunnel = stunnel daemonizes
+#
+stunnel /etc/stunnel/stunnel.conf
 
 chown root /var/spool/postfix
 chown root /var/spool/postfix/pid
@@ -22,15 +43,26 @@ MYNETWORK=$NETWORK/$PREFIX
 
 /usr/sbin/postconf smtputf8_enable=no
 /usr/sbin/postconf myhostname=$POSTFIX_HOSTNAME
-/usr/sbin/postconf relayhost="[$POSTFIX_RELAYHOST]"
 /usr/sbin/postconf mynetworks=$MYNETWORK
 /usr/sbin/postconf alias_database=/etc/aliases
 /usr/sbin/postconf alias_maps=hash:/etc/aliases
-
 newaliases
 
-if [ -n "$POSTFIX_AUTH_USERNAME" ] ; then
-    echo "[$POSTFIX_RELAYHOST] $POSTFIX_AUTH_USERNAME:$POSTFIX_AUTH_PASSWORD" > /etc/postfix/sasl_passwd
+if [ -n "$POSTFIX_RELAYHOST" ] ; then
+    /usr/sbin/postconf relayhost="$POSTFIX_RELAYHOST"
+fi
+
+if [ -n "$SES_AUTH_USERNAME" ] ; then
+    #
+    # setup relaying via AWS SES over stunnel
+    #
+    echo "$POSTFIX_RELAYHOST $SES_AUTH_USERNAME:$SES_AUTH_PASSWORD" > /etc/postfix/sasl_passwd
+    /usr/sbin/postconf smtp_sasl_auth_enable=yes
+    /usr/sbin/postconf smtp_sasl_security_options=noanonymous
+    /usr/sbin/postconf smtp_tls_security_level=may
+    /usr/sbin/postconf smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd
+elif [ -n "$POSTFIX_AUTH_USERNAME" ] ; then
+    echo "$POSTFIX_RELAYHOST $POSTFIX_AUTH_USERNAME:$POSTFIX_AUTH_PASSWORD" > /etc/postfix/sasl_passwd
     /usr/sbin/postmap /etc/postfix/sasl_passwd
     chown root:root /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
     chmod 0600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
@@ -39,6 +71,12 @@ if [ -n "$POSTFIX_AUTH_USERNAME" ] ; then
     /usr/sbin/postconf smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd
     /usr/sbin/postconf smtp_use_tls=yes
     /usr/sbin/postconf smtp_tls_CAfile=/etc/ssl/certs/ca-certificates.crt
+fi
+
+if [ -f /etc/postfix/sasl_passwd ] ; then
+        /usr/sbin/postmap /etc/postfix/sasl_passwd
+        chown root:root /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
+        chmod 0600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
 fi
 
 rm -f $POSTFIX_RUNDIR/pid/master.pid
