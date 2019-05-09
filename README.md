a80c19a2bb0f6e06d14a054a8e6bc18afd5116cc
Steve Williams <steve.williams@telxio.com>
2019-05-09 16:42:07 -0700

    Email Sending Failing on SES
    With the v3.x postfix the configuration needed to be changed to remove
    the external stunnel and configure internal tls and sasl support.
    
    Also the v3.x postfix supports a start-fg command to keep the mater
    daemon in the foreground.

--- README.md
+++ README.md
@@ -2,39 +2,76 @@
 
 Use in a docker compose environment to centralize email relaying to a smart host
 
-Use in conjunction with [Logger](//github.com/Cogosense/Logger) for centralized logging
+## Environment variables:
+
+POSTFIX\_HOSTNAME      - name of this postfix host
+                        must not be empty
+POSTFIX\_RELAYHOST     - name of the relay host with optional port
+                        relay.example.com[:587]
+                        must not be empty if AUTH credentials provided
+POSTFIX\_AUTH\_USERNAME - user name to authenticate with POSTFIX\_RELAYHOST
+POSTFIX\_AUTH\_PASSWORD - password for POSTFIX\_AUTH\_USERNAME
+SES\_AUTH\_USERNAME     - alternate to POSTFIX\_AUTH\_USERNAME
+SES\_AUTH\_PASSWORD     - alternate to POSTFIX\_AUTH\_PASSWORD
+
+
+If the SES AUTH alternates are used, the POSTFIX\_RELAYHOST is checked to ensure
+it is an SES relay host, and if the port spec is omitted, then port 587 is added.
+
+## Logging
+
+Postfix logs to the file /var/log/maillog
+
+## Running Locally
+
+The container can be tested by running it locally
+
+    make
+    docker run -p2525:25 \
+        -e POSTFIX_HOSTNAME=smtp.cogosense.com \
+        -e POSTFIX_RELAYHOST=email-smtp.us-west-2.amazonaws.com:587 \
+        -e POSTFIX_AUTH_USERNAME=<USERNAME> \
+        -e POSTFIX_AUTH_PASSWORD="<PASSWORD>" \
+        -v $PWD/log:/var/log \
+        -v $PWD/spool:/var/spool \
+        --rm \
+        053262612181.dkr.ecr.us-west-2.amazonaws.com/postfix:dev
+
+Different regions can be tested by changing the region name is the SES relayhost name.
+
+* email-smtp.us-west-2.amazonaws.com
+* email-smtp.us-east-1.amazonaws.com
+* email-smtp.eu-west-1.amazonaws.com
+
+Send an email using telnet (server responses are omitted):
+
+    telnet 0.0.0.0 2525
+    EHLO cogosense.com
+    MAIL FROM: <from@example.com>
+    RCPT TO: <to@another_domain.com>
+    DATA
+    Subject: Test Again
+
+    This is another test
+    .
+    quit
+
+## Docker Compose
 
 An example docker-compose.yml
 
-    volumes:
-        dev:
     services:
-        logger:
-            image: <registry>/logger
-            volumes:
-                - dev:/dev
-                - ./var/log:/var/log
         smtp:
             image: <registry>/postfix
             volumes:
-                - dev:/dev/logger
+                - ./log:/var/log
+                - ./spool:/var/spool
             environment:
-                POSTFIX_HOSTNAME: $POSTFIX_HOSTNAME
-                POSTFIX_RELAYHOST: $POSTFIX_RELAYHOST
-                POSTFIX_AUTH_USERNAME: $POSTFIX_AUTH_USERNAME
-                POSTFIX_AUTH_PASSWORD: $POSTFIX_AUTH_PASSWORD
-            depends_on:
-                - logger
+                POSTFIX_HOSTNAME: mailhost
+                POSTFIX_RELAYHOST: mailrelay.example.com:587
+                POSTFIX_AUTH_USERNAME: jimmy
+                POSTFIX_AUTH_PASSWORD: shushverysecret
         app:
             image: <registry>/app
             depends_on:
                 - smtp
-
-*POSTFIX\_AUTH\_USERNAME* and *POSTFIX\_AUTH\_PASSWORD* are optional and onlt required if the
-SMTP relay host requires SASL auth.
-
-If using logger, the following link must be created at runtime:
-
-    if [ -d /dev/logger ] ; then
-        ln -sf /dev/logger/log /dev/log
-    fi
--- src/postfix.sh
+++ src/postfix.sh
@@ -3,33 +3,32 @@ POSTFIX_RUNDIR=/var/spool/postfix
 [ -n "$POSTFIX_HOSTNAME" ] || { echo "set POSTFIX_HOSTNAME parameter in .env file" ; exit 1; }
 if [ -n "$SES_AUTH_USERNAME" ] ; then
     [ -n "$SES_AUTH_PASSWORD" ] || { echo "set POSTFIX_AUTH_PASSWORD parameter in .env file" ; exit 1; }
-    POSTFIX_RELAYHOST='127.0.0.1:2525'
+    [ -n "$POSTFIX_RELAYHOST" ] || { echo "set POSTFIX_RELAYHOST parameter in .env file" ; exit 1; }
+    case $POSTFIX_RELAYHOST in
+        email-smtp.*.amazonaws.com) POSTFIX_RELAYHOST=$POSTFIX_RELAYHOST:587;;
+        email-smtp.*.amazonaws.com:*) ;;
+        *) echo "POSTFIX_RELAYHOST is not an SES relay host" ; exit 1;;
+    esac
+    POSTFIX_AUTH_USERNAME="$SES_AUTH_USERNAME"
+    POSTFIX_AUTH_PASSWORD="$SES_AUTH_PASSWORD"
 else
     if [ -n "$POSTFIX_AUTH_USERNAME" ] ; then
         [ -n "$POSTFIX_AUTH_PASSWORD" ] || { echo "set POSTFIX_AUTH_PASSWORD parameter in .env file" ; exit 1; }
         [ -n "$POSTFIX_RELAYHOST" ] || { echo "set POSTFIX_RELAYHOST parameter in .env file" ; exit 1; }
     fi
-    case $POSTFIX_RELAYHOST in
-        '') ;;
-        *:*)
-            hostpart=${POSTFIX_RELAYHOST%%:*}
-            portpart=${POSTFIX_RELAYHOST#*:}
-            POSTFIX_RELAYHOST="[$hostpart]"${portpart:+":$portpart"}
-            ;;
-        *)
-            POSTFIX_RELAYHOST="[$POSTFIX_RELAYHOST]"
-            ;;
-    esac
 fi
 
-if [ -d /dev/logger ] ; then
-    ln -sf /dev/logger/log /dev/log
-fi
-
-#
-# start stunnel = stunnel daemonizes
-#
-stunnel /etc/stunnel/stunnel.conf
+case $POSTFIX_RELAYHOST in
+    '') ;;
+    *:*)
+        hostpart=${POSTFIX_RELAYHOST%%:*}
+        portpart=${POSTFIX_RELAYHOST#*:}
+        POSTFIX_RELAYHOST="[$hostpart]"${portpart:+":$portpart"}
+        ;;
+    *)
+        POSTFIX_RELAYHOST="[$POSTFIX_RELAYHOST]"
+        ;;
+esac
 
 chown root /var/spool/postfix
 chown root /var/spool/postfix/pid
@@ -41,6 +40,11 @@ eval `ipcalc -n $ipaddr`
 eval `ipcalc -p $ipaddr`
 MYNETWORK=$NETWORK/$PREFIX
 
+#
+# log to file
+#
+/usr/sbin/postconf maillog_file=/var/log/maillog
+
 /usr/sbin/postconf smtputf8_enable=no
 /usr/sbin/postconf myhostname=$POSTFIX_HOSTNAME
 /usr/sbin/postconf mynetworks=$MYNETWORK
@@ -50,27 +54,16 @@ newaliases
 
 if [ -n "$POSTFIX_RELAYHOST" ] ; then
     /usr/sbin/postconf relayhost="$POSTFIX_RELAYHOST"
-fi
-
-if [ -n "$SES_AUTH_USERNAME" ] ; then
-    #
-    # setup relaying via AWS SES over stunnel
-    #
-    echo "$POSTFIX_RELAYHOST $SES_AUTH_USERNAME:$SES_AUTH_PASSWORD" > /etc/postfix/sasl_passwd
-    /usr/sbin/postconf smtp_sasl_auth_enable=yes
-    /usr/sbin/postconf smtp_sasl_security_options=noanonymous
-    /usr/sbin/postconf smtp_tls_security_level=may
-    /usr/sbin/postconf smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd
-elif [ -n "$POSTFIX_AUTH_USERNAME" ] ; then
-    echo "$POSTFIX_RELAYHOST $POSTFIX_AUTH_USERNAME:$POSTFIX_AUTH_PASSWORD" > /etc/postfix/sasl_passwd
-    /usr/sbin/postmap /etc/postfix/sasl_passwd
-    chown root:root /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
-    chmod 0600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
-    /usr/sbin/postconf smtp_sasl_auth_enable=yes
-    /usr/sbin/postconf smtp_sasl_security_options=noanonymous
-    /usr/sbin/postconf smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd
-    /usr/sbin/postconf smtp_use_tls=yes
-    /usr/sbin/postconf smtp_tls_CAfile=/etc/ssl/certs/ca-certificates.crt
+    if [ -n "$POSTFIX_AUTH_USERNAME" ] ; then
+        /usr/sbin/postconf smtp_sasl_auth_enable=yes
+        /usr/sbin/postconf smtp_sasl_security_options=noanonymous
+        /usr/sbin/postconf smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd
+        /usr/sbin/postconf smtp_use_tls=yes
+        /usr/sbin/postconf smtp_tls_security_level=encrypt
+        /usr/sbin/postconf smtp_tls_note_starttls_offer=yes
+        /usr/sbin/postconf smtp_tls_CAfile=/etc/ssl/certs/ca-certificates.crt
+        echo "$POSTFIX_RELAYHOST $POSTFIX_AUTH_USERNAME:$POSTFIX_AUTH_PASSWORD" > /etc/postfix/sasl_passwd
+    fi
 fi
 
 if [ -f /etc/postfix/sasl_passwd ] ; then
@@ -79,21 +72,4 @@ if [ -f /etc/postfix/sasl_passwd ] ; then
         chmod 0600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
 fi
 
-rm -f $POSTFIX_RUNDIR/pid/master.pid
-/usr/sbin/postfix start
-if [ -f $POSTFIX_RUNDIR/pid/master.pid ] ; then
-    pid=`expr $(cat $POSTFIX_RUNDIR/pid/master.pid)`
-
-    if ! kill -0 $pid ; then
-        echo "postfix failed to start"
-        exit 1
-    fi
-    tail -f /proc/$pid/cmdline 2>&1 | while read line ; do
-        case "$line" in
-            *No\ such\ process)
-                echo "postfix exited"
-                break
-                ;;
-        esac
-    done
-fi
+/usr/sbin/postfix start-fg
